0553d29a3d903b8ed7f98edcd1a32869ff8c5c3b
zhanbao2000 <zhanbao2000@gmail.com>
2019-05-06 16:30:37 +0800

    Update 19040902.c

--- 19040902.c
+++ 19040902.c
@@ -3,13 +3,13 @@ int main(){
 	double a, b, m;
 	scanf("%lf%lf", &a, &b);
 	if(b==0.5){
-		m = a+a*0.01275;
+		m = a*0.01275;
 	}else{
 		switch((int)b){
-			case 1:	m = a+a*0.0275;
-			case 2:	m = a+a*0.067;
-			case 3:	m = a+a*0.12;
-			case 5: m = a+a*0.2375;
+			case 1:	m = a*0.0275;	break;
+			case 2:	m = a*0.067;	break;
+			case 3:	m = a*0.12;	break;
+			case 5: m = a*0.2375;	break;
 		}
 	}
 	printf("%.2lf\n", m);
