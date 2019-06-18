03828509499d49ad6c35a1beb7a4f6e3d67d0bf0
zhanbao2000 <zhanbao2000@gmail.com>
2019-06-18 09:36:19 +0800

    Update 19060402.c

--- 19060402.c
+++ 19060402.c
@@ -1,22 +1,27 @@
 #include<stdio.h>
 
-int *pmax(int *x, int *y){
-	return *x>*y?x:y;
-}
-
-int *pmin(int *x, int *y){
-	return *x<*y?x:y;
-}
-
 int main(){
-	int num[] = {1,8,10,2,-5,0,7,15,4,-5};
-	int *p = &num[0];
-	int i = 1, max = *p, min = *p;
+	int i = 0;
+	int pmax = 0, pmin = 0;
+	int num[9];
+	int max, min;
 	for(i;i<=9;i++){
-		p++;
-		if(*p>max){
-
+		int temp;
+		scanf("%d", &temp);
+		num[i] = temp;
+	}
+	max = num[0];
+	min = num[0];
+	for(i=1;i<=9;i++){
+		if(num[i]>max){
+			max = num[i];
+			pmax = i;
+		}
+		if(num[i]<min){
+			min = num[i];
+			pmin = i;
 		}
 	}
+	printf("最大数是%d,其下标是%d,最小数%d,其下标是%d\n", max, pmax, min, pmin);
 	return 0;
 }
